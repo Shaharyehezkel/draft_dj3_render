9d0828feb70d04e4b13284a3feb9ba6598bb6626
Shaharyehezkel <shahary315@gmail.com>
2023-11-08 12:17:46 +0200

    first commit

---
+++ super_django_p1/base/urls.py
@@ -0,0 +1,17 @@
+# appliactionnnnnnnnnnnnnnnnnnnnnnnnnnnn
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('register', views.register),
+    path('members', views.member_only ),
+    path('login/', views.MyTokenObtainPairView.as_view()),
+    path('products', views.Product_view.as_view()),
+    path('categories', views.Category_view.as_view()),
+]
+
+
+
+
